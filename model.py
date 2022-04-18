688b74b1e823ad3171f454190ec3238bcb11ee68
Yehoon Jang <jangyh0420@gmail.com>
2022-04-18 20:08:03 +0900

    commit

--- model.py
+++ model.py
@@ -1,1 +1,18 @@
+# Import PyTorch
 import torch
+from torch import nn
+
+class StyleTransferTransformer(nn.Module):
+    def __init__(self, encoder, tst_decoder):
+        super(StyleTransferTransformer, self).__init__()
+        self.encoder = encoder
+        self.tst_decoder = tst_decoder
+
+    def forward(self, src, trg):
+
+
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super(EncoderLayer, self).__init__()
+
+    def forward(self):
