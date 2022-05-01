6b49bc8ede5eb1593e9b58432045bb3187d22299
Yehoon Jang <jangyh0420@gmail.com>
2022-05-02 00:42:12 +0900

    nmt loss error

--- train.py
+++ train.py
@@ -1,14 +1,18 @@
 import tqdm
 import pickle
+import os
 
 import torch
-import tqdm
 from torch.utils.data import DataLoader
+
+import sentencepiece as spm
 
 from custom_dataset import CustomDataset
 from model import Encoder, TSTDecoder, NMTDecoder, StyleTransfer, StylizedNMT
 from loss import bce_loss, ce_loss, kl_loss
 
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+# os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 
 def tst_train_one_epoch(tst_model, epoch, data_loader, tst_optimizer, device):
     tst_model.train()
@@ -22,7 +26,7 @@ def tst_train_one_epoch(tst_model, epoch, data_loader, tst_optimizer, device):
             trg = trg.to(device)
             # size ; [16, 300]=[batch, max_len]
 
-            tst_out, total_latent, content_c, content_mu, content_logv, style_a, style_mu, style_logv = tst_model(src, trg)
+            tst_out, total_latent, content_c, content_mu, content_logv, style_a, style_mu, style_logv, output_list = tst_model(src, trg)
 
             # TODO Loss 재정의
             # BCE_loss = bce_loss(tst_out, trg)
@@ -48,7 +52,7 @@ def tst_train_one_epoch(tst_model, epoch, data_loader, tst_optimizer, device):
             tst_optimizer.step()    # Gradient Descent 시작
             pbar.update(1)
 
-    return train_loss/total, total_latent
+    return train_loss/total, total_latent, trg[:, 1:].tolist(), output_list
 
 def nmt_train_one_epoch(nmt_model, data_loader, nmt_optimizer, device):
     nmt_model.train()
@@ -79,11 +83,11 @@ def nmt_train_one_epoch(nmt_model, data_loader, nmt_optimizer, device):
             train_loss += loss_value
 
             nmt_optimizer.zero_grad()   # optimizer 초기화
-            loss.backward()
+            loss.backward(retain_graph=True)
             nmt_optimizer.step()    # Gradient Descent 시작
             pbar.update(1)
 
-    return train_loss/total
+    return train_loss/total, trg_trg, nmt_out
 
 @torch.no_grad()    #no autograd (backpropagation X)
 def tst_evaluate(tst_model, epoch, data_loader, device):
@@ -97,7 +101,7 @@ def tst_evaluate(tst_model, epoch, data_loader, device):
             src = src.to(device)
             trg = trg.to(device)
 
-            tst_out, total_latent, content_c, content_mu, content_logv, style_a, style_mu, style_logv = tst_model(src, trg)
+            tst_out, total_latent, content_c, content_mu, content_logv, style_a, style_mu, style_logv, output_list = tst_model(src, trg)
 
             # BCE_loss = ce_loss(tst_out, trg)
             # KL_loss, KL_weight = kl_loss(style_mu, style_logv, epoch, 0.0025, 2500)
@@ -116,10 +120,12 @@ def tst_evaluate(tst_model, epoch, data_loader, device):
 
             pbar.update(1)
 
-    return valid_loss/total
+    return valid_loss/total, trg[:, 1:].tolist(), output_list
 
 @torch.no_grad()    #no autograd (backpropagation X)
 def nmt_evaluate(nmt_model, data_loader, device):
+    target_list = []
+    output_list = []
     nmt_model.eval()
 
     valid_loss = 0.0
@@ -137,14 +143,16 @@ def nmt_evaluate(nmt_model, data_loader, device):
 
             trg_trg = trg[:, 1:].reshape(-1)
 
+
             CE_loss = ce_loss(nmt_out, trg_trg)
             loss = CE_loss
             loss_value = loss.item()
             valid_loss += loss_value
 
+
             pbar.update(1)
 
-    return valid_loss/total
+    return valid_loss/total, trg_trg, nmt_out
 
 def train():
     # Device Setting
@@ -184,7 +192,7 @@ def train():
 
     # TODO argparse
     min_len, max_len = 2, 300
-    batch_size = 200
+    batch_size = 250
     num_workers = 0
     tst_vocab_size = 1800
     nmt_vocab_size = 2400
@@ -193,9 +201,9 @@ def train():
     tst_valid_data = CustomDataset(em_informal_valid, em_formal_valid, min_len, max_len)
     tst_test_data = CustomDataset(em_informal_test, em_formal_test, min_len, max_len)
 
-    nmt_train_data = CustomDataset(pair_kor_train, pair_eng_train, min_len, max_len)
-    nmt_valid_data = CustomDataset(pair_kor_valid, pair_eng_valid, min_len, max_len)
-    nmt_test_data = CustomDataset(pair_kor_test, pair_eng_test, min_len, max_len)
+    nmt_train_data = CustomDataset(pair_eng_train, pair_kor_train, min_len, max_len)
+    nmt_valid_data = CustomDataset(pair_eng_valid, pair_kor_valid, min_len, max_len)
+    nmt_test_data = CustomDataset(pair_eng_test, pair_kor_test, min_len, max_len)
 
 
     tst_train_loader = DataLoader(tst_train_data, batch_size=batch_size, drop_last=True, shuffle=True,
@@ -214,39 +222,70 @@ def train():
 
 
     # TST Train
-    encoder = Encoder(input_size=tst_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
+    tst_encoder = Encoder(input_size=tst_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
     tst_decoder = TSTDecoder(output_size=tst_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
-    tst_model = StyleTransfer(encoder, tst_decoder, d_hidden=1024, style_ratio=0.3, device=device)
+    tst_model = StyleTransfer(tst_encoder, tst_decoder, d_hidden=1024, style_ratio=0.3, device=device)
     tst_model = tst_model.to(device)
 
     tst_optimizer = torch.optim.AdamW(tst_model.parameters(), lr=0.001)
 
     start_epoch = 0
-    epochs = 1
+    epochs = 0
+
     print("Start TST Training..")
+
+    tst_tokenizer = spm.SentencePieceProcessor()
+    tst_tokenizer.Load("/HDD/yehoon/data/tokenizer/train_em_formal_spm.model")
+    decode_output = []
     for epoch in range(start_epoch, epochs+1):
         print(f"Epoch: {epoch}")
-        epoch_loss, total_latent = tst_train_one_epoch(tst_model, epoch, tst_train_loader, tst_optimizer, device)
-        print(f"Training Loss: {epoch_loss:.5f}")
+        epoch_loss, total_latent, trg_list, out_list = tst_train_one_epoch(tst_model, epoch, tst_train_loader, tst_optimizer, device)
 
-        valid_loss = tst_evaluate(tst_model, epoch, tst_valid_loader, device)
+        print(f"Training Loss: {epoch_loss:.5f}")
+        # print(f"train target: {[tst_tokenizer.DecodeIds(i) for i in trg_list]}")
+        # print(f"train predict: {[tst_tokenizer.DecodeIds(j) for j in out_list]}")
+
+        valid_loss, trg_trg, val_out = tst_evaluate(tst_model, epoch, tst_valid_loader, device)
         print(f"Validation Loss: {valid_loss:.5f}")
 
+        # print(f"valid target: {[tst_tokenizer.DecodeIds(i) for i in trg_list]}")
+        # print(f"valid predict: {[tst_tokenizer.DecodeIds(j) for j in out_list]}")
+        target_decode = [tst_tokenizer.DecodeIds(i) for i in trg_list]
+        output_decoder = [tst_tokenizer.DecodeIds(j) for j in out_list]
+        decode_output.append((target_decode, output_decoder))
+
+    with open ("/HDD/yehoon/data/target_decode.txt", "w", encoding="utf8") as tf, open("/HDD/yehoon/data/output_decode.txt", "w", encoding="utf8") as of:
+        for t_line, o_line in decode_output:
+            for tline in t_line:
+                tf.write(f"{tline}\n")
+            for oline in o_line:
+                of.write(f"{oline}\n")
+        tf.close()
+        of.close()
+
+
     # NMT Train
-    nmt_decoder = NMTDecoder(output_size=tst_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
-    nmt_model = StylizedNMT(encoder, nmt_decoder, total_latent=total_latent, device=device)
+    nmt_encoder = Encoder(input_size=nmt_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
+    nmt_decoder = NMTDecoder(output_size=nmt_vocab_size, d_hidden=1024, d_embed=256, n_layers=2, dropout=0.1, device=device)
+    nmt_model = StylizedNMT(nmt_encoder, nmt_decoder, d_hidden=1024, total_latent=total_latent, device=device)
     nmt_model = nmt_model.to(device)
 
     nmt_optimizer = torch.optim.AdamW(nmt_model.parameters(), lr=0.001)
 
     start_epoch = 0
     epochs = 100
+
     print("Start NMT Training..")
+
+    nmt_tokenizer = spm.SentencePieceProcessor()
+    nmt_tokenizer.Load("/HDD/yehoon/data/tokenizer/train_pair_kor_spm.model")
     for epoch in range(start_epoch, epochs + 1):
         print(f"Epoch: {epoch}")
-        epoch_loss = nmt_train_one_epoch(nmt_model, nmt_train_loader, nmt_optimizer, device)
+        epoch_loss , trg_trg, train_out= nmt_train_one_epoch(nmt_model, nmt_train_loader, nmt_optimizer, device)
         print(f"Training Loss: {epoch_loss:.5f}")
+        # print(f"train target: {nmt_tokenizer.DecodeIds(trg_trg)}, train predict: {nmt_tokenizer.DecodeIds(train_out)}")
 
-        valid_loss = nmt_evaluate(nmt_model, nmt_valid_loader, device)
+        valid_loss, trg_trg, val_out = nmt_evaluate(nmt_model, nmt_valid_loader, device)
         print(f"Validation Loss: {valid_loss:.5f}")
+        print(f"valid target: {nmt_tokenizer.DecodeIds(trg_trg)}, valid predict: {nmt_tokenizer.DecodeIds(val_out)}")
 
