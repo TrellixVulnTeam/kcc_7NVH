a5c0d248278b7f698d3149f95821ba77a74e3f30
YeHoonJang <jangyh0420@gmail.com>
2022-05-12 21:14:55 +0900

    edit kl loss

--- loss.py
+++ loss.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 
 import numpy as np
 
@@ -12,10 +13,15 @@ def bce_loss(out, target):
     BCE_loss = BCE(out, target)
     return BCE_loss
 
-def kl_loss(mu, logv, epoch, k, xo):
-    KL_loss = -0.5 * torch.sum(1 + logv - mu.pow(2) - logv.exp())
-    KL_weight = kl_anneal_function(epoch, k, xo)
-    return KL_loss, KL_weight
+# def kl_loss(mu, logv, epoch, k, xo):
+#     KL_loss = -0.5 * torch.sum(1 + logv - mu.pow(2) - logv.exp())
+#     KL_weight = kl_anneal_function(epoch, k, xo)
+#     return KL_loss, KL_weight
+
+def kl_loss(out, target, mean, logv, trg_pad_idx):
+    reproduction_loss = F.cross_entropy(out, target, ignore_index=trg_pad_idx)
+    KLD = -0.5 * torch.sum(1 + logv - mean.pow(2) - logv.exp())
+    return reproduction_loss + KLD
 
 def ce_loss(out, target):
     CE = nn.CrossEntropyLoss(ignore_index=0)
